0d4dca6aae1fdf2720ebd5173e769afd060de945
zwan1312 <zwan1312@bu.edu>
2020-10-10 13:51:21 -0400

    static rendering

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,45 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
       <section className={utilStyles.headingMd}>
         <p>Hello there! I am a BA/MS in Computer Science student at Boston University, expecting to graduate in 2022.</p>
         <p>I have a keen interest in cybersecurity and software engineering for social good.</p>
         <p>I have taken several course relating to cybersecurity such as cryptography and network security, and am volunteering to develop software for organizations with a positive social impact.</p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
